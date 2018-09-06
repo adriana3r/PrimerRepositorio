3611a93efc24c57a18eeef99f361d1b3d595141c
adriana3r <adriana3r@gmail.com>
2018-09-06 11:34:19 -0300

    Prueba del primer repositorio.

---
+++ src/primerrepositorio/PrimerRepositorio.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerrepositorio;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PrimerRepositorio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("Creación del primer repositorio.");
+        System.out.println("https://github.com/adriana3r/PrimerRepositorio.git");
+        System.out.println("Netbeans/Team/Git/InitializeRepository");
+        System.out.println("Proyecto/Git/Add");
+        System.out.println("Proyecto/Git/Commit");
+        System.out.println("Proyecto/Git/Remote/Pull");
+    }
+    
+}
